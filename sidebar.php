f3e6abf38bc2c3a7271ec29830c5e92acc0b6a4e
kamaliaa1301-boop <kamaliaa1301@gmail.com>
2026-06-28 07:34:21 +0700

    Create sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,8 @@
+<div class="sidebar">
+    <h2 class="logo">S1 MAHASISWA</h2>
+
+    <a href="dashboard.php">🏠 Dashboard</a>
+    <a href="daftar.php">📋 Data Mahasiswa</a>
+    <a href="tambah.php">➕ Tambah Mahasiswa</a>
+    <a href="logout.php">🚪 Logout</a>
+</div>
